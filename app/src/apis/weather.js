6cf52e3b3d447d7724e7107d840860e8bca35e1a
“Calvin <Calvinli0824@gmail.com>
2023-04-13 13:38:00 -0400

    added weather api to the Request.js file

--- app/src/apis/weather.js
+++ app/src/apis/weather.js
@@ -1,13 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Axios from 'axios'
-import { response } from 'express'
 
-export function Weather() {
-    const weather = () => {
-        Axios.get("http://api.weatherstack.com/current?access_key=7163a01cc6c0ff276e49d7e5553e22c2&query=New_York&units=f").then(
-            (response) => {
-                console.log(response);
-            }
-        )
-    }
+export async function Weather() {
+    const [ weather, setWeather ] = useState([]); 
+    
+    const weathers = await Axios.get("http://api.weatherapi.com/v1/forecast.json?key=a&q=New_York&days=3&aqi=no&alerts=no");
+    setWeather(weathers.data.results);
+    console.log(weather);
 }
--- app/src/pages/Request.js
+++ app/src/pages/Request.js
@@ -1,9 +1,24 @@
 import NavBar from "../components/NavBar.js";
-import { Weather } from "../apis/weather.js";
-import React from "react";
+import React, { useState } from 'react'
+import Axios from 'axios'
 
 
 export default function Request() {
+
+    const [ weather, setWeather ] = useState([]); 
+
+    async function Weather() {
+        try {
+            const weathers = await Axios.get(
+                "http://api.weatherapi.com/v1/forecast.json?key=40de535bf85e4305b02183509231004&q=New_York&days=7&aqi=no&alerts=no"
+                );
+            setWeather(weathers);
+            console.log(weather);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
     return (
         <>
             <NavBar/>
